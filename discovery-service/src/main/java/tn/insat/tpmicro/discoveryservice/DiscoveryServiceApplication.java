d97ffc8944736b4804f2ceaea549f12be4ca6e12
Sana KHIARI <khiarisanaa@gmail.com>
2017-12-04 20:59:49 +0100

    commit read me

---
+++ discovery-service/src/main/java/tn/insat/tpmicro/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,17 @@
+package tn.insat.tpmicro.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+}
